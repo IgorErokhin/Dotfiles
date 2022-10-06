688c33de6590eb6b7d269be8c687c24ac9c93149
Igor Erokhin <igor.gero@gmail.com>
2022-10-06 16:22:36 +0300

    Add notepad++ files

--- dotfiles-get.ps1
+++ dotfiles-get.ps1
@@ -1,2 +1,4 @@
-Copy-Item ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Destination .
-Copy-Item ~\.gitconfig -Destination .
+Copy-Item ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Destination . -Force
+Copy-Item ~\.gitconfig -Destination . -Force
+Copy-Item $Env:APPDATA\Notepad++\config.xml -Destination . -Force
+Copy-Item $Env:APPDATA\Notepad++\userDefineLangs -Destination . -Force
--- dotfiles-set.ps1
+++ dotfiles-set.ps1
@@ -1,2 +1,3 @@
 Copy-Item Microsoft.PowerShell_profile.ps1 -Destination ~\Documents\PowerShell
 Copy-Item .gitconfig -Destination ~
+Copy-Item Notepad++ -Destination $Env:APPDATA -Force
